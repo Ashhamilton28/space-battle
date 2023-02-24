352f9f5e8972ba1e16f6b33a4070400fe8d06b6c
Ashley <Ashhamilton22888@gmail.com>
2023-02-24 11:53:26 -0500

    added chatbot with prompt for playerName, alert and window confirm for the initial dialogue

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-const player = "Asha"
+
+
+//chatbot with initial dialogue
+
+//prompt for name
+let playerName = prompt("Hello! what is your name?!")
+alert(`I'm glad your here ${playerName}! We have been invaded by aliens and we need you to help us!`)
+//ask player to help defeat aliens to start game
+window.confirm(`Will you help us defeat the aliens ${playerName}?`)
+
+
 
 //using constructors to build alien ships
-
-
 // formula to get a random whole number between a range is Math.floor(Math.random() * (maximum -minimum +1)) + minimum;
 class alienShip {
     constructor(hull, firepower, accuracy) {
@@ -11,8 +19,8 @@ class alienShip {
         this.accuracy = (Math.random() * .6 - .8 + 1) + .8
 
     }
-    attack(){
-        console.log("Asha has been attacked")
+    attack() {
+        console.log(`${playerName} has been attacked!`)
     }
 }
 
@@ -26,4 +34,17 @@ const alienShip6 = new alienShip
 
 console.log(alienShip1.attack)
 
+//The player spaceship
+class ussAssembly {
+    constructor(hull, firepower, accuracy) {
+        this.hull = 20
+        this.firepower = 5
+        this.accuracy = .7
+    }
+    attackAliens(){
+        console.log(`${playerName} has attacked the aliens!`)
+    }
+}
+const ussAssembly1 = new ussAssembly
+console.log(ussAssembly1.attackAliens)
 
